10a128dab29ae441336e0326a8a2cb8ca4414fa9
omar369 <omarins.222@gmail.com>
2020-11-26 13:26:56 -0600

    styles.css added - body margin 0

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,5 @@
 //app de three js
-
+console.log("commit 3 - primer cambio")
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
