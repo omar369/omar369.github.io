2c7d00959e646df4fe0eeeb7b1a7dfbbeddde844
omar369 <omarins.222@gmail.com>
2020-11-26 13:34:08 -0600

    Adding style and resize in three js

--- docs/main.js
+++ docs/main.js
@@ -8,6 +8,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+window.addEventListener('resize', ()=>{
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = (window.innerWidth / window.innerHeight);
+    camera.updateProjectMatrix();
+})
+
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
